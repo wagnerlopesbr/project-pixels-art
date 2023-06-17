76ed58e5351250043e3ca7fd342a1ee0baafcf64
Wagner Lopes <wagnerlopesbr@hotmail.com>
2023-06-17 15:18:16 -0300

    feat: WL - Pixels Art 3/10 v1

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ const pixelBoard = document.querySelector('#pixel-board');
 for (let i = 1; i <= 25; i += 1) {
   pixelBoard.innerHTML += "<li class='pixel'></li>";
 }
+
+function selectingColor() {
+  for (let selectedColor of colorList) {
+    function pickAColor(event) {
+      const selected = document.querySelector('.selected');
+      if (selected) {
+        selected.classList.remove('selected');
+      } else {
+        event.target.classList.add('selected');
+      }
+    }
+    selectedColor.addEventListener('click', pickAColor);
+  }
+}
+selectingColor();
