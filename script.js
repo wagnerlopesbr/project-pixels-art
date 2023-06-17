1e61ad07745fd6a32c390481e1f0966e047217fb
Wagner Lopes <wagnerlopesbr@hotmail.com>
2023-06-17 13:31:26 -0300

    feat: WL - Pixels Art 1/10 v1

---
+++ script.js
@@ -0,0 +1,5 @@
+const colorList = document.querySelectorAll('.color');
+colorList[0].style.backgroundColor = 'blue';
+colorList[1].style.backgroundColor = 'red';
+colorList[2].style.backgroundColor = 'yellow';
+colorList[3].style.backgroundColor = 'black';
