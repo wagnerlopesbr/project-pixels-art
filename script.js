e19722a93b865550793fd5e6d96317247a3857b9
Wagner Lopes <wagnerlopesbr@hotmail.com>
2023-06-17 20:50:19 -0300

    feat: WL - Pixels Art 4/10 vAMÉM

--- script.js
+++ script.js
@@ -3,22 +3,38 @@ colorList[0].style.backgroundColor = 'blue';
 colorList[1].style.backgroundColor = 'red';
 colorList[2].style.backgroundColor = 'yellow';
 colorList[3].style.backgroundColor = 'black';
+colorList[0].id = 'colorBlue';
+colorList[1].id = 'colorRed';
+colorList[2].id = 'colorYellow';
+colorList[3].id = 'colorBlack';
 
 const pixelBoard = document.querySelector('#pixel-board');
-for (let i = 1; i <= 25; i += 1) {
+for (let i = 0; i < 25; i += 1) {
   pixelBoard.innerHTML += "<li class='pixel'></li>";
 }
 
 function selectingColor() {
-  for (let selectedColor of colorList) {
+  for (let selectColor of colorList) {
     function pickAColor(event) {
       const selected = document.querySelector('.selected');
       if (selected) {
         selected.classList.remove('selected');
       }
       event.target.classList.add('selected');
+      currentColor = event.target.style.backgroundColor;
     }
-    selectedColor.addEventListener('click', pickAColor);
+    selectColor.addEventListener('click', pickAColor);
   }
 }
 selectingColor();
+
+const pixels = document.querySelectorAll('.pixel');
+function coloringPixels(newPixelBGC) {
+  const colorToUse = document.querySelector('.selected');
+  newPixelBGC.style.backgroundColor = colorToUse.style.backgroundColor;
+}
+for (let i = 0; i < pixels.length; i += 1) {
+  pixels[i].addEventListener('click', function coloring() {
+    coloringPixels(pixels[i])});
+}
+coloringPixels();
